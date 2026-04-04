712f6c33a34ecc0d8d92f7cd1690d44428f7d2f3
CapRodri24 <201905170@est.umss.edu>
2026-04-04 16:21:52 -0400

    ultimo intento google

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -11,23 +11,18 @@ class EmailService {
     }
     
     // Configuración mejorada
-    this.transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+   this.transporter = nodemailer.createTransport({
+      host: "142.250.27.108",
       port: 465,
       secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
-      family: 4,
-      connectionTimeout: 30000,
-      greetingTimeout: 30000,
-      socketTimeout: 30000,
       tls: {
-        family: 4,
-        rejectUnauthorized: false
+        servername: "smtp.gmail.com"
       }
-    })
+    });
   }
 
   async getAgentesEmails() {
